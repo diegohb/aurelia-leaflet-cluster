e75bd74defe4ffa9628659692cc001864ce6d017
diegohb <diegohb@bustamantenow.com>
2017-08-08 17:30:03 -0400

    added test suite for GeoCoordinate and test for toString()

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -1,7 +1,17 @@
+import { GeoCoordinate } from "../../src/model/GeoCoordinate";
 import {App} from '../../src/app';
 
 describe('the app', () => {
   it('says hello', () => {
     expect(new App().message).toBe('Hello World!');
+  });
+});
+
+describe('a GeoCoordinate object', () => {
+  it("display name is formatted as (x,y)", () => {
+    const lat = 125
+    const lng = -75;
+    const expectedDisplayName = `(${lat},${lng})`;
+    expect(new GeoCoordinate(lat, lng).toString()).toBe(expectedDisplayName);
   });
 });
