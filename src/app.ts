b46930c2ea66a399d58653326091d48734738bd2
diegohb <diegohb@bustamantenow.com>
2017-08-22 20:32:39 -0400

    set base layers and exposed bound prop to view for control

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,27 @@ export class App {
     maxZoom: 18,
     zoomControl: true,
     center: {
-      lat:38.883,
+      lat: 38.883,
       lng: -76.9947
     }
+  }
+   public leafletLayers:any =
+    {
+      base: [
+      {
+        id: "wmflabs.streets.bw",
+        url: "https://api.mapbox.com/styles/v1/compsagnathus/cj4rhzsvw2xb82sn12xvq12fq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29tcHNhZ25hdGh1cyIsImEiOiJjajRyZ3ZpbzUwazJhMzRqcmxhZmpsd281In0.gDi4uI4dBfOxHJWedZnkXQ",
+        type: "tile",
+        options: {
+          maxZoom: 18,
+          attribution:
+          "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>" +
+          "© <a href='http://www.openstreetmap.org/copyright'> OpenStreetMap </a> " +
+          "<strong> <a href='https://www.mapbox.com/feedback/' target='_blank'> Improve this map </a></strong >"
+        }
+      }
+    ],
+    overlay: []
   };
 
   constructor() {
@@ -25,35 +43,8 @@ export class App {
 
   public async attached(): Promise<any> {
     $("p#jquery").text("jquery loaded!")
-    
-    let elMap: HTMLElement = $(".leaflet-container").get(0);
 
     return Promise.resolve();
-  }
-
-  private async initializeMap(pLeafletMap: Map, pMaxZoom: number): Promise<any> {
-
-    addZoomControls(pLeafletMap);
-
-    let baseLayer =
-      tileLayer(
-        "https://api.mapbox.com/styles/v1/compsagnathus/cj4rhzsvw2xb82sn12xvq12fq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29tcHNhZ25hdGh1cyIsImEiOiJjajRyZ3ZpbzUwazJhMzRqcmxhZmpsd281In0.gDi4uI4dBfOxHJWedZnkXQ",
-        {
-          maxZoom: pMaxZoom,
-          attribution:
-          "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a>" +
-          "© <a href='http://www.openstreetmap.org/copyright'> OpenStreetMap </a> " +
-          "<strong> <a href='https://www.mapbox.com/feedback/' target='_blank'> Improve this map </a></strong >",
-          id: "wmflabs.streets.bw"
-        });
-
-    baseLayer.addTo(pLeafletMap);
-    return pLeafletMap;
-
-    function addZoomControls(pMap: Map): void {
-      let zoomControl = control.zoom({ position: "topleft" });
-      pMap.addControl(zoomControl);
-    }
   }
 
   private static async getMapExtentBounds(pMap: Map): Promise<GeographicExtent> {
